87e5e06ebb92f85e3e525523618466e76aee034f
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 23:03:45 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,36 +1,3 @@
-# Оголошення змінних
-variable "github_token" {
-  description = "GitHub token for authentication"
-  type        = string
-  sensitive   = true
-}
-
-variable "github_owner" {
-  description = "GitHub owner (username or organization)"
-  type        = string
-}
-
-variable "repository_name" {
-  description = "Name of the GitHub repository"
-  type        = string
-}
-
-# Налаштування провайдера GitHub
-provider "github" {
-  token = var.github_token
-  owner = var.github_owner
-}
-
-# Створення репозиторію
-resource "github_repository" "repo" {
-  name               = var.repository_name
-  description        = "Terraform managed repository"
-  visibility         = "private"
-  auto_init          = true
-  license_template   = "mit"
-  gitignore_template = "Terraform"
-}
-
 # Створення гілки develop
 resource "github_branch" "develop" {
   repository = github_repository.repo.name
@@ -41,18 +8,3 @@ resource "github_branch" "develop" {
 resource "github_branch_default" "default" {
   repository = github_repository.repo.name
   branch     = github_branch.develop.branch
-}
-
-# Додавання користувача softservedata як співавтора
-resource "github_repository_collaborator" "collaborator" {
-  repository = github_repository.repo.name
-  username   = "softservedata"
-  permission = "push"
-}
-
-# Збереження Terraform коду у секреті репозиторію
-resource "github_actions_secret" "terraform" {
-  repository      = github_repository.repo.name
-  secret_name     = "TERRAFORM"
-  plaintext_value = file("main.tf")
-}
