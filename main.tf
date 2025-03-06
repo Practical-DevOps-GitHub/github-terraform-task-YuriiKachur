7b9f3cdb0161166fa866c49714ddd6861eaa7f06
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-07 00:17:43 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
-resource "github_branch_default" "develop" {
-  repository = github_repository.repo.name
+resource "default branch" {
   branch     = "develop"  # Створення гілки develop
-}
-
-output "default_branch" {
-  value = github_repository.repo.default_branch  # Повертаємо дефолтну гілку
 }
