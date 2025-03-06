ef227c70a21d5b094e5e002af363bde1a4cf5a8e
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-07 00:01:52 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,27 +19,8 @@ variable "repository_name" {
   type        = string
 }
 
-# Create the `develop` branch using local-exec
-resource "null_resource" "create_develop_branch" {
-  provisioner "local-exec" {
-    command = <<EOT
-      git clone https://${var.github_token}@github.com/${var.github_owner}/${var.repository_name}.git
-      cd ${var.repository_name}
-      git checkout -b develop
-      git push origin develop
-    EOT
-  }
-}
-
-# Set the default branch to `develop`
-resource "github_branch_default" "default" {
+# Create the `develop` branch in the existing repository
+resource "github_branch" "develop" {
   repository = var.repository_name
   branch     = "develop"
-}
-
-# Add `softservedata` as a collaborator
-resource "github_repository_collaborator" "collaborator" {
-  repository = var.repository_name
-  username   = "softservedata"
-  permission = "push"
 }
