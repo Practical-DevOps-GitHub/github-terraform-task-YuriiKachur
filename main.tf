01b61030b8a44974838c2175f81c49ecfd3a0e31
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 23:50:50 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,34 +15,25 @@ variable "github_owner" {
 }
 
 variable "repository_name" {
-  description = "Name of the GitHub repository"
+  description = "Name of the existing GitHub repository"
   type        = string
-  default     = "example-repo"
-}
-
-# Create the repository
-resource "github_repository" "repo" {
-  name               = var.repository_name
-  description        = "Example repository with Terraform"
-  visibility         = "private"
-  auto_init          = true
 }
 
 # Set the default branch to `develop`
 resource "github_branch_default" "default" {
-  repository = github_repository.repo.name
+  repository = var.repository_name
   branch     = "develop"
 }
 
 # Create the `develop` branch
 resource "github_branch" "develop" {
-  repository = github_repository.repo.name
+  repository = var.repository_name
   branch     = "develop"
 }
 
 # Add `softservedata` as a collaborator
 resource "github_repository_collaborator" "collaborator" {
-  repository = github_repository.repo.name
+  repository = var.repository_name
   username   = "softservedata"
   permission = "push"
 }
