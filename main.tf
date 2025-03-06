51bfd07f3e1af0b8da91410ba8b2efd0182061ed
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 23:25:18 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,7 @@ provider "github" {
   token = var.github_token
 }
 
-variable "repository_name" {
-  description = "The name of the GitHub repository"
-  type        = string
-}
-
-resource "github_repository" "repo" {
-  name           = var.repository_name
-  visibility     = "private"
-  default_branch = "develop"
-}
-
-resource "github_repository_collaborator" "collaborator" {
-  repository = github_repository.repo.name
-  username   = "softservedata"
-  permission = "push"  # Adjust permissions as needed
-}
-
-resource "github_branch_protection" "develop_protection" {
-  repository = github_repository.repo.name
+resource "github_branch_default" "default_branch" {
+  repository = var.repository_name
   branch     = "develop"
-  required_pull_request_reviews {
-    required_approving_review_count = 2
-  }
 }
