12ae8e6f41cba2b6310b316027280388e280d133
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 23:26:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,6 @@ provider "github" {
   token = var.github_token
 }
 
-resource "github_branch_default" "default_branch" {
-  repository = var.repository_name
-  branch     = "develop"
+resource 'github_branch_default' {
+  branch     = 'develop'
 }
