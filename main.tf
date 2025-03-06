53d32699d87b19e7479abc23783002f824a1c712
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 23:13:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,30 @@
-# Створення гілки develop
+provider "github" {
+  token = var.github_token
+}
+
+variable "repository_name" {
+  description = "The name of the GitHub repository"
+  type        = string
+}
+
+resource "github_repository" "repo" {
+  name           = var.repository_name
+  visibility     = "private"
+  default_branch = "develop"
+}
+
 resource "github_branch" "develop" {
   repository = github_repository.repo.name
   branch     = "develop"
 }
 
-# Встановлення гілки develop як гілки за замовчуванням
-resource "github_branch_default" "default" {
+resource "github_repository_collaborator" "collaborator" {
   repository = github_repository.repo.name
-  branch     = github_branch.develop.branch
+  username   = "softservedata"
+  permission = "push"
+}
+
+variable "github_token" {
+  description = "GitHub Token"
+  type        = string
 }
