76140f867df1a4e7b640f87bc76293e10074b5ff
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-07 00:05:51 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,40 +1,19 @@
 provider "github" {
-  token = var.github_token
-  owner = var.github_owner
-}
-
-variable "github_token" {
-  description = "GitHub Personal Access Token"
-  type        = string
-  sensitive   = true
-}
-
-variable "github_owner" {
-  description = "GitHub organization or user"
-  type        = string
-}
-
-variable "repository_name" {
-  description = "Name of the existing GitHub repository"
-  type        = string
 }
 
 # Set the default branch to `develop`
 resource "github_branch_default" "default" {
-  repository = var.repository_name
   branch     = "develop"
 }
 
 # Add `softservedata` as a collaborator
 resource "github_repository_collaborator" "collaborator" {
-  repository = var.repository_name
   username   = "softservedata"
   permission = "push"
 }
 
 # Protect the `develop` branch
 resource "github_branch_protection" "develop" {
-  repository_id = var.repository_name
   pattern       = "develop"
 
   required_pull_request_reviews {
@@ -48,7 +27,6 @@ resource "github_branch_protection" "develop" {
 
 # Protect the `main` branch
 resource "github_branch_protection" "main" {
-  repository_id = var.repository_name
   pattern       = "main"
 
   required_pull_request_reviews {
@@ -69,7 +47,6 @@ resource "github_actions_secret" "pat" {
 
 # Add a deploy key
 resource "github_repository_deploy_key" "deploy_key" {
-  repository = var.repository_name
   title      = "DEPLOY_KEY"
   key        = var.deploy_key
   read_only  = false
