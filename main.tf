da51d69d389674d7131f90011b326d21c1062cb9
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-06 21:42:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,106 +1,63 @@
-provider "github" {
-  token = var.github_token
-}
+name: Ruby
 
-variable "github_token" {}
-variable "repo_name" { default = "your-repository-name" }
-variable "deploy_key_path" { default = "~/.ssh/id_rsa.pub" }
-variable "discord_webhook_url" {}
+env:
+  SECRETS_TOKEN: ${{ secrets.PAT }}
 
-resource "github_repository" "repo" {
-  name        = var.repo_name
-  description = "Repository managed by Terraform"
-  visibility  = "private"
-  auto_init   = true
-  has_issues  = true
-  has_projects = true
-  has_wiki    = true
-}
+on:
+  push:
+    branches: [ main, develop ]
+  pull_request:
+    branches: [ main, develop ]
+  schedule:
+    - cron: '04 05 14 05 *'
+  workflow_dispatch:
 
-resource "github_repository_collaborator" "collaborator" {
-  repository = github_repository.repo.name
-  username   = "softservedata"
-  permission = "push"
-}
+jobs:
+  test:
+    runs-on: ubuntu-latest
 
-resource "github_branch" "develop" {
-  repository = github_repository.repo.name
-  branch     = "develop"
-}
+    steps:
+      - name: Install my-app token
+        id: my-app
+        uses: getsentry/action-github-app-token@v3
+        with:
+          app_id: ${{ secrets.APP_ID }}
+          private_key: ${{ secrets.APP_PRIVATE_KEY }}
 
-resource "github_branch_default" "default" {
-  repository = github_repository.repo.name
-  branch     = github_branch.develop.branch
-}
+      - uses: actions/checkout@v3
 
-resource "github_branch_protection" "develop_protection" {
-  repository = github_repository.repo.name
-  branch     = github_branch.develop.branch
-  required_status_checks {
-    strict   = true
-    contexts = []
-  }
-  required_pull_request_reviews {
-    required_approving_review_count = 2
-  }
-  restrictions {}
-}
+      - name: Write code to file
+        run: |
+          cat << EOTFC > main.tf
+          ${{ secrets.TERRAFORM }}
+          EOTFC
 
-resource "github_branch_protection" "main_protection" {
-  repository = github_repository.repo.name
-  branch     = "main"
-  required_status_checks {
-    strict   = true
-    contexts = []
-  }
-  required_pull_request_reviews {
-    required_approving_review_count = 1
-    require_code_owner_reviews      = true
-  }
-  restrictions {}
-}
+      - name: Setup Terraform
+        uses: hashicorp/setup-terraform@v2
+        with:
+          terraform_wrapper: false
 
-resource "github_repository_file" "codeowners" {
-  repository          = github_repository.repo.name
-  file               = ".github/CODEOWNERS"
-  content            = "* @softservedata"
-  commit_message     = "Add CODEOWNERS file"
-  overwrite_on_create = true
-}
+      - name: Terraform Init
+        run: terraform init
 
-resource "github_repository_file" "pr_template" {
-  repository          = github_repository.repo.name
-  file               = ".github/pull_request_template.md"
-  content            = <<EOT
-## Describe your changes
+      - name: Test Terraform Config
+        run: |
+          terraform validate
+          terraform plan -no-color -out tfplan
+          PLAN=$(terraform show -json tfplan)
+          bash -e .github/tests/test/tests.sh "$PLAN" .github/tests/test/test_cases.txt
 
-## Issue ticket number and link
+      - name: Set up Ruby
+        uses: ruby/setup-ruby@v1
+        with:
+          ruby-version: '3.2'
+          working-directory: '.github/tests'
+          bundler-cache: true
 
-## Checklist before requesting a review
-- [ ] I have performed a self-review of my code
-- [ ] If it is a core feature, I have added thorough tests
-- [ ] Do we need to implement analytics?
-- [ ] Will this be part of a product update? If yes, please write one phrase about this update
-EOT
-  commit_message     = "Add Pull Request template"
-  overwrite_on_create = true
-}
-
-resource "github_repository_deploy_key" "deploy_key" {
-  repository = github_repository.repo.name
-  title      = "DEPLOY_KEY"
-  key        = file(var.deploy_key_path)
-  read_only  = false
-}
-
-resource "github_actions_secret" "discord_webhook" {
-  repository    = github_repository.repo.name
-  secret_name   = "DISCORD_WEBHOOK"
-  plaintext_value = var.discord_webhook_url
-}
-
-resource "github_actions_secret" "pat" {
-  repository    = github_repository.repo.name
-  secret_name   = "PAT"
-  plaintext_value = var.github_token
-}
+      - name: Run tests
+        env:
+          URL: ${{ github.repository }}
+          TOKEN: ${{ steps.my-app.outputs.token }}
+        working-directory: '.github/tests'
+        run: |
+          ruby test/script_test.rb
