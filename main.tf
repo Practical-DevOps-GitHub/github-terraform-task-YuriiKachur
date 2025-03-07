ec4988a7ad4d46c53ac5f16974fd8c66b4c7a719
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2025-03-07 15:05:13 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+  }
+}
+
 provider "github" {
   token = var.github_token
   owner = var.github_owner
